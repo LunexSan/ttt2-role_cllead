4ff5ac9bee59b4342c39375a6088a3fee36c07ed
Blueberryy <igory.ygr200@gmail.com>
2020-10-03 12:40:10 +0500

    Create clleadeng.lua

---
+++ lua/lang/russian/clleadeng.lua
@@ -0,0 +1,11 @@
+L = LANG.GetLanguageTableReference("Русский")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[CULTLEADER.name] = "Лидер культа"
+L["info_popup_" .. CULTLEADER.name] = [[Вы лидер культа, работайте со своим культом!]]
+L["body_found_" .. CULTLEADER.abbr] = "Он был лидером культа."
+L["search_role_" .. CULTLEADER.abbr] = "Этот человек был лидером культа!"
+L["target_" .. CULTLEADER.name] = "Лидер культа"
+L["ttt2_desc_" .. CULTLEADER.name] = [[Лидер культа - центральная фигура культа, помогает вашему культу расти и обеспечивает вашу собственную безопасность, чтобы безраздельно властвовать.]]
+
+-- OTHER ROLE LANGUAGE STRINGS
